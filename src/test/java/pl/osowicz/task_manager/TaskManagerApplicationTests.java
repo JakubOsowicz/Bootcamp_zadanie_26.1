be31dfe061be0354443ef5f795f72ce1bbcf75e8
Jakub Osowicz <jakub.osowicz@wp.pl>
2021-04-27 17:21:36 +0200

    TaskManager - stworzenie reporytorium

---
+++ src/test/java/pl/osowicz/task_manager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.osowicz.task_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
